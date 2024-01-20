3558ee0be48b68ecf7629f46ee771d358b8d79cf
varshith264 <chandurivarshith264@gmail.com>
2024-01-21 00:49:25 +0530

    add FirebaseTokenFilter

--- src/main/java/com/service/SpringStarter1Application.java
+++ src/main/java/com/service/SpringStarter1Application.java
@@ -2,11 +2,10 @@ package com.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
 public class SpringStarter1Application {
--- src/main/java/com/service/configuration/security/FirebaseTokenFilter.java
+++ src/main/java/com/service/configuration/security/FirebaseTokenFilter.java
@@ -1,41 +1,45 @@
-//package com.service.security;
-//
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseToken;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.ServletException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//import java.util.Objects;
-//
-//public class FirebaseTokenFilter extends OncePerRequestFilter {
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-//                                    FilterChain filterChain) throws ServletException, IOException {
-//        String token = request.getHeader("Authorization");
-//        if (token != null && token.startsWith("Bearer ")) {
-//            token = token.substring(7); // Remove "Bearer " prefix
-//        }
-//
-//        if(Objects.nonNull(token)) {
-//            // Validate the Firebase JWT token
-//            try {
-//                FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(token);
-//
-//                // Set Firebase UID as a request header
-//                request.setAttribute("Firebase-UID", firebaseToken.getUid());
-//
-//                // Continue with the filter chain
-//                filterChain.doFilter(request, response);
-//            } catch (Exception e) {
-//                // Token validation failed
-//                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid token");
-//            }
-//        }
-//    }
-//}
+package com.service.configuration.security;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+@Slf4j
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+public class FirebaseTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws IOException, ServletException {
+        log.info("executing do filter internal");
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        if(Objects.nonNull(token)) {
+            try {
+                FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(token);
+                request.setAttribute("Firebase-UID", firebaseToken.getUid());
+                filterChain.doFilter(request, response);
+            } catch (Exception e) {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "Invalid token");
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
